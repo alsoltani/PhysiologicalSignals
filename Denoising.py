de40d08b1de11dd30fd3720edaa6a2830512c45f
Parveez <alain.soltni@ensae.fr>
2014-03-18 15:14:49 +0100

    5th

--- Denoising.py
+++ Denoising.py
@@ -56,8 +56,7 @@ if __name__ == '__main__':
     plt.ylabel('MEG')
     plt.legend(loc='lower right')
     plt.tight_layout()
-    plt.show()
-    
+
     ######### 4) COMPARING MP & OMP RESULTS #########
     
     z_omp_stat, err_omp_stat, n_omp_stat = omp_stat_criterion(BasisT.T, BasisT, y2, math.sqrt(np.var(y1)))
@@ -77,13 +76,14 @@ if __name__ == '__main__':
     plt.ylabel('MEG')
     plt.legend(loc='lower right')
     plt.tight_layout()
-    plt.show()
     
-    ######### 4) COMPARING CLASSIC & ORTHONORMALIZED DICTIONARIES #########
+    ######### 5) COMPARING CLASSIC & ORTHONORMALIZED DICTIONARIES #########
     
     GBasisT = gram_schmidt(PhiT.dot(np.identity(y2.size)).T).T
     GBasisT /= np.sqrt(np.sum(GBasisT ** 2, axis=0))
-    
+    #GBasisT = np.linalg.qr(PhiT.dot(np.identity(y2.size)))[0]
+    #print GBasisT.dot(GBasisT.T)
+
     z_omp_gstat, err_omp_gstat, n_omp_gstat = omp_stat_criterion(GBasisT.T, GBasisT, y2, math.sqrt(np.var(y1)))
 
     fig = plt.figure()
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     plt.ylabel('MEG')
     plt.legend(loc='lower right')
     plt.tight_layout()
-    plt.show()
+
 
     ######### 6) LOG-DECREMENT RESULTS #########
     
@@ -168,25 +168,81 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.show()
 
+    """
     ######### 7) OMP, HARD THRESHOLDING COMPARISON #########
 
     #Gabriel Peyré's article states that MP /w Orthogonal Basis is equiv. to Hard Thresholding.
+    #The follwing thrshold is arbitrary.
 
     z_thr = hard_thresholding_operator(GBasisT.T, GBasisT, y2, 2e-12)
 
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
     ax1.plot(y2, '#3D3D29', label='Noisy signal - during experiment')
-    ax1.plot(z_thr, 'y', label='Denoised signal: Hard Thresholding. - MAD Threshold.')
+    ax1.plot(z_thr, 'y', label='Denoised signal: Hard Thresholding.')
     plt.xlabel('Time (ms)')
     plt.ylabel('MEG')
     plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
     plt.legend(loc='lower right')
     ax2 = fig.add_subplot(212)
     ax2.plot(z_omp_gstat, 'r', label='Denoised signal : OMP stat, orthonorm. dict. : ' + str(n_omp_gstat) + ' iter.')
-    ax2.plot(z_thr, 'y', label='Denoised signal: Hard Thresholding. - MAD Threshold.')
+    ax2.plot(z_thr, 'y', label='Denoised signal: Hard Thresholding')
     plt.xlabel('Time (ms)')
     plt.ylabel('MEG')
     plt.legend(loc='lower right')
     plt.tight_layout()
     plt.show()
+    """
+    ######### 8) OMP, HARD THRESHOLDING USING EARLY EXPERIMENT #########
+
+    y1_resized = np.zeros(y2.shape)
+    y1_resized[:180] = y1[:180]
+    y1_resized[180:256] = y1[:76]
+    z_thr = hard_thresholding_early_experiment(GBasisT.T, GBasisT, y1_resized, y2)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.plot(y2, '#3D3D29', label='Noisy signal - during experiment')
+    ax1.plot(z_thr, 'y', label='Denoised signal: Hard Thresholding. - Pre-exp. threshold.')
+    plt.xlabel('Time (ms)')
+    plt.ylabel('MEG')
+    plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
+    plt.legend(loc='lower right')
+    ax2 = fig.add_subplot(212)
+    ax2.plot(z_omp_gstat, 'r', label='Denoised signal : OMP stat, orthonorm. dict. : ' + str(n_omp_gstat) + ' iter.')
+    ax2.plot(z_thr, 'y', label='Denoised signal: Hard Thresholding')
+    plt.xlabel('Time (ms)')
+    plt.ylabel('MEG')
+    plt.legend(loc='lower right')
+    plt.tight_layout()
+    plt.show()
+
+    """
+    ######### 9) TESTING OUR MODEL #########
+
+    #TESTING OUR MODEL : NOISE SIMULATION
+    #Our idea is the following : we simulate a white noise, add it to the denoised signal
+    #and run the algorithms to find a new denoised signal. If the two signals match,
+    #the algorithm is considered as efficient.
+
+    noise = np.asarray(np.random.normal(0, np.var(y1), y2.shape[0])).T
+
+    z_new = z_mp_stat+noise
+    z_new_mp_stat, err_new_mp_stat, n_new_mp_stat = mp_stat_criterion(BasisT.T, BasisT, z_new, math.sqrt(np.var(y1)))
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.plot(y2, '#999966', label='Noisy signal - during experiment')
+    ax1.plot(z_mp_stat, '#993300', label='Denoised signal: MP stat, classic dict. : ' + str(n_mp_stat) + ' iter.')
+    ax1.plot(z_new_mp_stat, '#FF0066', label='Re-denoised signal: same charact. : ' + str(n_new_mp_stat) + ' iter.')
+    plt.xlabel('Time (ms)')
+    plt.ylabel('MEG')
+    plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
+    plt.legend(loc='lower right')
+    ax2 = fig.add_subplot(212)
+    ax2.plot(z_new_mp_stat-z_mp_stat, '#009999', label='Difference between the 2 denoised signals.')
+    plt.xlabel('Time (ms)')
+    plt.ylabel('MEG')
+    plt.legend(loc='lower right')
+    plt.tight_layout()
+    plt.show()"""
--- Functions.py
+++ Functions.py
@@ -6,7 +6,6 @@ from math import sqrt
 
 ######### MATCHING PURSUIT, NO CRITERION #########
 
-
 def mp_arbitrary_criterion(phi, phi_t, y, n_iter):
     x = np.zeros(phi_t.dot(y).shape[0])
     err = [np.linalg.norm(y)]
@@ -23,8 +22,7 @@ def mp_arbitrary_criterion(phi, phi_t, y, n_iter):
     return z, err, n_iter
 
 
-######### MATCHING PURSUIT, STATISTICAL CRITERION ######### 
-
+######### MATCHING PURSUIT, STATISTICAL CRITERION #########
 
 def mp_stat_criterion(phi, phi_t, y, sigma):
     p = phi_t.dot(y).shape[0]
@@ -52,7 +50,7 @@ def mp_stat_criterion(phi, phi_t, y, sigma):
 
         x = phi.dot(np.identity(p)[:, i_0])
         h = dic_arr.dot(np.linalg.inv(dic_arr.T.dot(dic_arr))).dot(dic_arr.T)
-        t = np.abs(x.T.dot(y))/(sigma*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
+        t = np.abs(x.T.dot(y-h.dot(y)))/(sigma*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
 
         s = np.random.standard_t(int(y.shape[0]-dic_arr.shape[1])-1, 50)
         q = np.percentile(s, 95)
@@ -64,8 +62,7 @@ def mp_stat_criterion(phi, phi_t, y, sigma):
     return phi.dot(sparse), err, dic_arr.shape[1]
 
 
-######### ORTHOGONAL MATCHING PURSUIT, STAT CRITERION ######### 
-
+######### ORTHOGONAL MATCHING PURSUIT, STAT CRITERION #########
 
 def omp_stat_criterion(phi, phi_t, y, sigma):
     p = phi_t.dot(y).shape[0]
@@ -93,7 +90,7 @@ def omp_stat_criterion(phi, phi_t, y, sigma):
 
         x = phi.dot(np.identity(p)[:, i_0])
         h = dic_arr.dot(np.linalg.inv(dic_arr.T.dot(dic_arr))).dot(dic_arr.T)
-        t = np.abs(x.T.dot(y))/(sigma*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
+        t = np.abs(x.T.dot(y-h.dot(y)))/(sigma*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
 
         s = np.random.standard_t(int(y.shape[0]-dic_arr.shape[1])-1, 50)
         q = np.percentile(s, 95)
@@ -109,7 +106,6 @@ def omp_stat_criterion(phi, phi_t, y, sigma):
 
 #consistent estimator of the std deviation
 #where hat(Sigma) a consistent estimator of the std dev. of the finest level detail coefficients.
-
 
 def mad(data):
     return np.ma.median(np.abs(data - np.ma.median(data)))
@@ -138,3 +134,53 @@ def hard_thresholding_operator(phi, phi_t, y, thresh):
     c = phi_t.dot(y)
     c = pywt.thresholding.hard(c, value=thresh)
     return phi.dot(c)
+
+#c) Hard Thresholding, using MP on pre-experiment noise :
+# - We select atoms describing noise implementing a MP on y1 (signal pre-experiment)
+# - we operate a hard thresholding by removing those from our explanatory model
+
+#ONLY WORKS FOR TWO SIGNALS OF IDENTICAL SIZES.
+
+
+def hard_thresholding_early_experiment(phi, phi_t, early_signal, currnt_signal):
+    if early_signal.shape[0] == currnt_signal.shape[0]:
+
+        p = phi_t.dot(early_signal).shape[0]
+        sigma = float(sqrt(np.var(early_signal)))
+        #sigma = 1e-20
+        sparse = np.zeros(p)
+        indexes = []
+
+        c = phi_t.dot(early_signal)
+        abs_c = np.abs(c)
+        i_0 = np.argmax(abs_c)
+        indexes.append(i_0)
+
+        dic = [phi.dot(np.identity(p)[:, i_0])]
+        dic_arr = np.asarray(dic).T
+        t, q = (2, 1)
+
+        while t > q:
+
+            sparse[i_0] += c[i_0]
+
+            c = phi_t.dot(early_signal - phi.dot(sparse))
+            abs_c = np.abs(c)
+            i_0 = np.argmax(abs_c)
+            indexes.append(i_0)
+
+            x = phi.dot(np.identity(p)[:, i_0])
+            h = dic_arr.dot(np.linalg.inv(dic_arr.T.dot(dic_arr))).dot(dic_arr.T)
+            t = np.abs(x.T.dot(early_signal-h.dot(early_signal)))/(sigma*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
+
+            s = np.random.standard_t(int(early_signal.shape[0]-dic_arr.shape[1])-1, 50)
+            q = np.percentile(s, 95)
+
+            dic.append(x)
+            dic_arr = np.asarray(dic).T
+
+        indexes.pop()
+        return currnt_signal-phi.dot(sparse)
+
+    else:
+        return "Shapes dot not match. The present algorithm cannot be implemented."
