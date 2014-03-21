eb6db9ac2ff4d032326d1e3efdfeb937cd12c6a6
Parveez <alain.soltni@ensae.fr>
2014-03-21 17:18:49 +0100

    7th

--- Denoising.py
+++ Denoising.py
@@ -3,218 +3,231 @@ import math
 from Functions import *
 from Utility import *
 from Classes import *
+import matplotlib.pyplot as plt
+#from mpltools import style
+#style.use('ggplot')
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+######### 1) CHARGING DATA #########
 
-    ######### 1) CHARGING DATA #########
+data = np.load("data_cond1.npy")
+times = np.load("times_cond1.npy")
 
-    data = np.load("data_cond1.npy")
-    times = np.load("times_cond1.npy")
+times *= 1e3
+timesD = times[180:360]  # We only denoise between 0 and 300 sec, during the experiment
+timesD1 = times[:180]
 
-    times *= 1e3
-    timesD = times[180:360]  # We only denoise between 0 and 300 sec, during the experiment
-    timesD1 = times[:180]
+y = data[100, :]
+y1 = y[:180]  # Raw signal before the experiment
+y2 = y[180:180+256]  # signal during the experiment
 
-    y = data[100, :]
-    y1 = y[:180]  # Raw signal before the experiment
-    y2 = y[180:180+256]  # signal during the experiment
-
-    ######### 2) SETTING DICTIONARY PARAMETERS #########
+######### 2) SETTING DICTIONARY PARAMETERS #########
     
-    wave_name = 'db6'
-    wave_level = 5
-    PhiT = DictT(level=wave_level, name=wave_name)
-    Phi = Dict(sizes=PhiT.sizes, name=PhiT.name)
+wave_name = 'db6'
+wave_level = 5
+PhiT = DictT(level=wave_level, name=wave_name)
+Phi = Dict(sizes=PhiT.sizes, name=PhiT.name)
 
-    BasisT = PhiT.dot(np.identity(y2.size))
-    BasisT /= np.sqrt(np.sum(BasisT ** 2, axis=0))
+BasisT = PhiT.dot(np.identity(y2.size))
+BasisT /= np.sqrt(np.sum(BasisT ** 2, axis=0))
 
-    ######### 3) COMPARING MP RESULTS, W/ & W/O STATISTICAL CRITERION #########
+######### 3) COMPARING MP RESULTS, W/ & W/O STATISTICAL CRITERION #########
 
-    z_mp_arb, err_mp_arb, n_mp_arb = mp_arbitrary_criterion(BasisT.T, BasisT, y2, 20)
+z_mp_arb, err_mp_arb, n_mp_arb = mp_arbitrary_criterion(BasisT.T, BasisT, y2, 15)
 
-    #plt.figure()
-    #plt.matshow(BasisT.dot(BasisT.T))
-    #plt.colorbar()
-    #plt.show()
+#plt.figure()
+#plt.matshow(BasisT.dot(BasisT.T))
+#plt.colorbar()
+#plt.show()
 
-    z_mp_stat, err_mp_stat, n_mp_stat = mp_stat_criterion(BasisT.T, BasisT, y2, math.sqrt(np.var(y1)))
+z_mp_stat, err_mp_stat, n_mp_stat = mp_stat_criterion(BasisT.T, BasisT, y2, math.sqrt(np.var(y1)))
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax1.plot(y2, '#3D3D29', label='Noisy signal - during experiment')
-    ax1.plot(z_mp_stat, 'r', label='Denoised signal : MP, stat criterion')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
-    plt.legend(loc='lower right')
-    ax2 = fig.add_subplot(212)
-    ax2.plot(z_mp_arb, 'b', label='Denoised signal: MP, no criterion : ' + str(n_mp_arb) + ' iter.')
-    ax2.plot(z_mp_stat, 'r', label='Denoised signal : MP, stat criterion : ' + str(n_mp_stat) + ' iter.')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.legend(loc='lower right')
-    plt.tight_layout()
+fig = plt.figure(1)
+ax1 = fig.add_subplot(211, xlabel='Time (ms)', ylabel='MEG')
+ax1.plot(y2, '#585858', label='Noisy signal - during experiment')
+ax1.plot(z_mp_stat, '#FE2E2E', lw=2, label='Denoised: MP, statistical criterion')
+plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
 
-    ######### 4) COMPARING MP & OMP RESULTS #########
+ax2 = fig.add_subplot(212, xlabel='Time (ms)', ylabel='MEG')
+ax2.plot(z_mp_stat, '#FE2E2E', lw=2, label='Denoised: MP, statistical criterion : ' + str(n_mp_stat) + ' iter.')
+ax2.plot(z_mp_arb, '#1C1C1C', ls='--', lw=3, label='Denoised: MP, no criterion : ' + str(n_mp_arb) + ' iter.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
+plt.tight_layout()
+
+######### 4) COMPARING MP & OMP RESULTS #########
+
+z_omp_stat, err_omp_stat, n_omp_stat = omp_stat_criterion(BasisT.T, BasisT, y2, math.sqrt(np.var(y1)))
+
+fig = plt.figure(2)
+ax1 = fig.add_subplot(211, xlabel='Time (ms)', ylabel='MEG')
+ax1.plot(y2, '#585858', label='Noisy - during experiment')
+ax1.plot(z_omp_stat, '#6699FF', lw=2, label='Denoised: OMP : ' + str(n_omp_stat) + ' iter.')
+ax1.plot(z_mp_stat, '#B20000', ls='--', lw=3, label='Denoised: MP : ' + str(n_mp_stat) + ' iter.')
+plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
+
+ax2 = fig.add_subplot(212, xlabel='Atoms included', ylabel='Log-error')
+ax2.plot(map(math.log, err_omp_stat), '#6699FF', lw=2, label='OMP')
+ax2.plot(map(math.log, err_mp_stat), '#B20000', ls='--', lw=3, label='MP')
+plt.title('Log-decrement results')
+plt.legend(loc='lower right', fontsize=11)
+plt.tight_layout()
+
+######### 5) COMPARING CLASSIC & ORTHONORMALIZED DICTIONARIES #########
     
-    z_omp_stat, err_omp_stat, n_omp_stat = omp_stat_criterion(BasisT.T, BasisT, y2, math.sqrt(np.var(y1)))
+GBasisT = gram_schmidt(PhiT.dot(np.identity(y2.size)).T).T
+GBasisT /= np.sqrt(np.sum(GBasisT ** 2, axis=0))
+#GBasisT = np.linalg.qr(PhiT.dot(np.identity(y2.size)))[0]
+#print GBasisT.dot(GBasisT.T)
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax1.plot(y2, '#3D3D29', label='Noisy signal - during experiment')
-    ax1.plot(z_omp_stat, '#009933', label='Denoised signal: OMP, stat criterion')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
-    plt.legend(loc='lower right')
-    ax2 = fig.add_subplot(212)
-    ax2.plot(z_mp_stat, 'r', label='Denoised signal : MP, stat criterion : ' + str(n_mp_stat) + ' iter.')
-    ax2.plot(z_omp_stat, '#009933', label='Denoised signal : OMP, stat criterion : ' + str(n_omp_stat) + ' iter.')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.legend(loc='lower right')
-    plt.tight_layout()
+z_omp_gstat, err_omp_gstat, n_omp_gstat = omp_stat_criterion(GBasisT.T, GBasisT, y2, math.sqrt(np.var(y1)))
 
-    ######### 5) COMPARING CLASSIC & ORTHONORMALIZED DICTIONARIES #########
+fig = plt.figure()
+ax1 = fig.add_subplot(211, xlabel='Time (ms)', ylabel='MEG')
+ax1.plot(y2, '#585858', label='Noisy signal - during experiment')
+ax1.plot(z_omp_gstat, '#FF6699', linewidth=2, label='Denoised: OMP, orthonorm. dic :' + str(n_omp_gstat) + ' iter.')
+ax1.plot(z_omp_stat, '#002E8A', ls='--', lw=3, label='Denoised: OMP, classic dic : ' + str(n_omp_stat) + ' iter.')
+plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
+
+ax2 = fig.add_subplot(212, xlabel='Atoms included', ylabel='Log-Error')
+ax2.plot(map(math.log, err_omp_gstat), '#FF6699', linewidth=2, label='OMP, orthonorm. dic')
+ax2.plot(map(math.log, err_omp_stat), '#002E8A', ls='--', lw=3, label='OMP, classic dic')
+plt.legend(loc='lower right', fontsize=11)
+plt.tight_layout()
+
+######### 6) LOG-DECREMENT RESULTS #########
     
-    GBasisT = gram_schmidt(PhiT.dot(np.identity(y2.size)).T).T
-    GBasisT /= np.sqrt(np.sum(GBasisT ** 2, axis=0))
-    #GBasisT = np.linalg.qr(PhiT.dot(np.identity(y2.size)))[0]
-    #print GBasisT.dot(GBasisT.T)
+#Let us compare wavelet dictionaries within the Daubechies family,
+#using classic and orthonormalized dictionaries.
 
-    z_omp_gstat, err_omp_gstat, n_omp_gstat = omp_stat_criterion(GBasisT.T, GBasisT, y2, math.sqrt(np.var(y1)))
+fig = plt.figure()
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax1.plot(y2, '#3D3D29', label='Noisy signal - during experiment')
-    ax1.plot(z_omp_gstat, '#A319A3', label='Denoised signal: OMP stat, Gram-Schmidt.')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
-    plt.legend(loc='lower right')
-    ax2 = fig.add_subplot(212)
-    ax2.plot(z_omp_gstat, '#A319A3', label='Denoised signal : OMP stat, Gram-Schmidt : ' + str(n_omp_gstat) + ' iter.')
-    ax2.plot(z_omp_stat, '#009933', label='Denoised signal : OMP stat, w/o Gram-Sch. : ' + str(n_omp_stat) + ' iter.')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.legend(loc='lower right')
-    plt.tight_layout()
+# - CLASSIC DB DICTIONARY
 
-    ######### 6) LOG-DECREMENT RESULTS #########
-    
-    #6.1) Insights : Which algorithm, dictionary is the best ?
-    #Our criterion here is the log-decrement of the error term.
+ax1 = fig.add_subplot(211, xlabel='Atoms included', ylabel='Log-Error')
+for i in xrange(1, 7):
+    WaveT = DictT(level=5, name='db'+str(i))
+    MatT = WaveT.dot(np.identity(y2.size))
+    MatT /= np.sqrt(np.sum(MatT ** 2, axis=0))
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax1.plot(map(math.log, err_mp_stat), 'r', label='MP stat, w/o Gram-Schmidt.')
-    ax1.plot(map(math.log, err_omp_stat), '#009933', label='OMP stat, w/o Gram-Schmidt.')
-    plt.xlabel('Atoms included')
-    plt.ylabel('Log-Error')
-    plt.title('Selecting the right algorithm : MP, OMP.')
-    plt.legend(loc='lower right')
-    ax2 = fig.add_subplot(212)
-    ax2.plot(map(math.log, err_omp_gstat), '#A319A3', label='OMP stat, Gram-Schmidt.')
-    ax2.plot(map(math.log, err_omp_stat), '#009933', label='OMP stat, w/o Gram-Sch.')
-    plt.xlabel('Atoms included')
-    plt.ylabel('Log-Error')
-    plt.title('Selecting the right dictionary : orthogonalization results.')
-    plt.legend(loc='lower right')
-    plt.tight_layout()
+    Error = omp_stat_criterion(MatT.T, MatT, y2, math.sqrt(np.var(y1)))[1]
+    ax1.plot(map(math.log, Error), lw=2, label='db'+str(i))
 
-    #6.2) Let us compare wavelets with the Daubechies family,
-    # using classic and orthonormalized dictionaries.
+plt.title('Log-Error decrement results - w/o Gram-Schmidt.')
+plt.legend(loc='lower right', fontsize=11)
 
-    fig = plt.figure()
+# - ORTHONORMAL DB DICTIONARY
 
-    # - CLASSIC DB DICTIONARY
+ax2 = fig.add_subplot(212, xlabel='Atoms included', ylabel='Log-Error')
+for i in xrange(1, 7):
+    WaveT = DictT(level=5, name='db'+str(i))
+    MatT = gram_schmidt(WaveT.dot(np.identity(y2.size)).T).T
+    MatT /= np.sqrt(np.sum(MatT ** 2, axis=0))
 
-    ax1 = fig.add_subplot(211)
-    for i in xrange(1, 7):
-        WaveT = DictT(level=5, name='db'+str(i))
-        Wave = Dict(sizes=PhiT.sizes, name=PhiT.name)
-        MatT = WaveT.dot(np.identity(y2.size))
-        MatT /= np.sqrt(np.sum(MatT ** 2, axis=0))
+    Error = omp_stat_criterion(MatT.T, MatT, y2, math.sqrt(np.var(y1)))[1]
+    ax2.plot(map(math.log, Error), lw=2, label='db'+str(i))
 
-        Error = omp_stat_criterion(MatT.T, MatT, y2, math.sqrt(np.var(y1)))[1]
-        ax1.plot(map(math.log, Error), label='db'+str(i))
+plt.title('Log-Error decrement results - w/ Gram-Schmidt.')
+plt.legend(loc='lower right', fontsize=11)
 
-    plt.xlabel('Atoms included')
-    plt.ylabel('Log-Error')
-    plt.title('Log-Error decrement results - w/o Gram-Schmidt.')
-    plt.legend(loc='lower right')
+plt.tight_layout()
 
-    # - ORTHONORMAL DB DICTIONARY
+######### 7) OMP, HARD THRESHOLDING COMPARISON #########
 
-    ax2 = fig.add_subplot(212)
-    for i in xrange(1, 7):
-        WaveT = DictT(level=5, name='db'+str(i))
-        Wave = Dict(sizes=PhiT.sizes, name=PhiT.name)
-        MatT = gram_schmidt(WaveT.dot(np.identity(y2.size)).T).T
-        MatT /= np.sqrt(np.sum(MatT ** 2, axis=0))
+#Gabriel Peyre's article states that MP /w can be equiv. to Hard Thresholding,
+#when the decomposition basis is a square orthonormal matrix.
+#Hard Thresholding has to verify same sparsity constraint as the Matching Pursuit solution.
 
-        Error = omp_stat_criterion(MatT.T, MatT, y2, math.sqrt(np.var(y1)))[1]
-        ax2.plot(map(math.log, Error), label='db'+str(i))
+z_thr = hard_thresholding(GBasisT.T, GBasisT, y2, n_omp_gstat)
 
-    plt.xlabel('Atoms included')
-    plt.ylabel('Log-Error')
-    plt.title('Log-Error decrement results - w/ Gram-Schmidt.')
-    plt.legend(loc='lower right')
+fig = plt.figure()
+ax1 = fig.add_subplot(211, xlabel='Time (ms)', ylabel='MEG')
+ax1.plot(y2, '#585858', label='Noisy signal - during experiment')
+ax1.plot(z_thr, '#FF9933', lw=2, label='Denoised: Hard Thresholding.')
+plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
 
-    plt.tight_layout()
+ax2 = fig.add_subplot(212, xlabel='Time (ms)', ylabel='MEG')
+ax2.plot(z_thr, '#FF9933', lw=2, label='Denoised: Hard Thresholding.')
+ax2.plot(z_omp_gstat, '#A30052', ls='--', lw=3, label='Denoised: OMP, orthonorm. dict: ' + str(n_omp_gstat) + ' iter.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
+plt.tight_layout()
 
-    ######### 7) OMP, HARD THRESHOLDING COMPARISON #########
+######### 8) TESTING OUR MODEL #########
 
-    #Gabriel Peyre's article states that MP /w can be equiv. to Hard Thresholding,
-    #when the decomposition basis is a square orthonormal matrix.
-    #Hard Thresholding has to verify same sparsity constraint as the Matching Pursuit solution.
+#TESTING OUR MODEL : NOISE SIMULATION
+#Our idea is the following : we simulate a white noise, add it to the denoised signal
+#and run the algorithms to find a new denoised signal. If the two signals match,
+#the algorithm is considered as efficient.
 
-    z_thr = hard_thresholding(GBasisT.T, GBasisT, y2, n_omp_gstat)
+noise = np.asarray(np.random.normal(0, np.var(y1), y2.shape[0])).T
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax1.plot(y2, '#3D3D29', label='Noisy signal - during experiment')
-    ax1.plot(z_thr, '#CC00FF', label='Denoised signal: Hard Thresholding.')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
-    plt.legend(loc='lower right')
-    ax2 = fig.add_subplot(212)
-    ax2.plot(z_omp_gstat, 'r', label='Denoised signal : OMP stat, orth. dict. : ' + str(n_omp_gstat) + ' iter.')
-    ax2.plot(z_thr, '#CC00FF', label='Denoised signal: Hard Thresholding.')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.legend(loc='lower right')
-    plt.tight_layout()
+z_new = z_mp_stat+noise
+z_new_stat, err_new_stat, n_new_stat = mp_stat_criterion(BasisT.T, BasisT, z_new, math.sqrt(np.var(y1)))
 
-    ######### 8) TESTING OUR MODEL #########
+fig = plt.figure()
+ax1 = fig.add_subplot(211, xlabel='Time (ms)', ylabel='MEG')
+ax1.plot(y2, '#585858', label='Noisy signal - during experiment')
+ax1.plot(z_new_stat, '#33FF33', lw=2, label='Re-denoised: same charact. : ' + str(n_new_stat) + ' iter.')
+ax1.plot(z_mp_stat, '#660000', ls='--', lw=3, label='Denoised: MP, classic dict. : ' + str(n_mp_stat) + ' iter.')
+plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
 
-    #TESTING OUR MODEL : NOISE SIMULATION
-    #Our idea is the following : we simulate a white noise, add it to the denoised signal
-    #and run the algorithms to find a new denoised signal. If the two signals match,
-    #the algorithm is considered as efficient.
+ax2 = fig.add_subplot(212, xlabel='Time (ms)', ylabel='MEG')
+ax2.plot(z_new_stat-z_mp_stat, '#585858', lw=2, label='Difference between the 2 denoised signals.')
+plt.xlabel('Time (ms)')
+plt.ylabel('MEG')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
+plt.tight_layout()
 
-    noise = np.asarray(np.random.normal(0, np.var(y1), y2.shape[0])).T
+######### 9) ENDOGENOUS VARIANCE #########
 
-    z_new = z_mp_stat+noise
-    z_new_stat, err_new_stat, n_new_stat = mp_stat_criterion(BasisT.T, BasisT, z_new, math.sqrt(np.var(y1)))
+#We refine our model by considering variance as an endogenous variable,
+#using OLS estimator s².
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax1.plot(y2, '#999966', label='Noisy signal - during experiment')
-    ax1.plot(z_mp_stat, 'b', label='Denoised signal: MP stat, classic dict. : ' + str(n_mp_stat) + ' iter.')
-    ax1.plot(z_new_stat, 'r', label='Re-denoised signal: same charact. : ' + str(n_new_stat) + ' iter.')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
-    plt.legend(loc='lower right')
-    ax2 = fig.add_subplot(212)
-    ax2.plot(z_new_stat-z_mp_stat, '#009999', label='Difference between the 2 denoised signals.')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('MEG')
-    plt.legend(loc='lower right')
-    plt.tight_layout()
+z_mp_end, err_mp_end, n_mp_end = mp_stat_endogen_var(BasisT.T, BasisT, y2)
+z_omp_end, err_omp_end, n_omp_end = omp_stat_endogen_var(BasisT.T, BasisT, y2)
 
-    plt.show()
+fig = plt.figure()
+ax1 = fig.add_subplot(211, xlabel='Time (ms)', ylabel='MEG')
+ax1.plot(y2, '#585858', label='Noisy - during experiment')
+ax1.plot(z_mp_end, '#FF66CC', lw=2, label='Denoised: MP, endo. variance : ' + str(n_mp_end) + ' iter.')
+ax1.plot(z_mp_stat, '#660066', ls='--', lw=3, label='Denoised: MP, exog. variance : ' + str(n_mp_stat) + ' iter.')
+plt.title('Signal denoising - ' + wave_name + ' wavelets, ' + str(wave_level) + '-level decomposition.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
+
+ax2 = fig.add_subplot(212, xlabel='Time (ms)', ylabel='MEG')
+ax2.plot(y2, '#585858', label='Noisy - during experiment')
+ax2.plot(z_omp_end, '#FF9966', lw=2, label='Denoised: OMP, endo. variance : ' + str(n_omp_end) + ' iter.')
+ax2.plot(z_omp_stat, '#8F2400', ls='--', lw=3, label='Denoised: OMP, exog. variance : ' + str(n_omp_stat) + ' iter.')
+plt.legend(loc='lower right', fontsize=11)
+plt.axis([0, 256, -5e-12, 5e-12])
+
+plt.tight_layout()
+
+#Associated log-decrement results
+
+fig = plt.figure()
+ax1 = fig.add_subplot(211, xlabel='Atoms included', ylabel='Log-Error')
+ax1.plot(map(math.log, err_mp_end), '#FF66CC', lw=2, label='MP, endo. var.')
+ax1.plot(map(math.log, err_mp_stat), '#660066', ls='--', lw=3, label='MP, exog. var.')
+plt.title('Log-decrement results')
+plt.legend(loc='lower right', fontsize=11)
+
+ax2 = fig.add_subplot(212, xlabel='Atoms included', ylabel='Log-Error')
+ax2.plot(map(math.log, err_omp_end), '#FF9966', lw=2, label='OMP stat, endo. var.')
+ax2.plot(map(math.log, err_omp_stat), '#8F2400', ls='--', lw=3, label='OMP stat, exog. var.')
+plt.legend(loc='lower right', fontsize=11)
+plt.tight_layout()
+
+plt.show()
--- Functions.py
+++ Functions.py
@@ -1,10 +1,18 @@
 #coding:latin_1
 import numpy as np
 import pywt
-from math import sqrt, copysign
+from scipy.stats import norm
+from math import sqrt
 
 
-######### MATCHING PURSUIT, NO CRITERION #########
+######### MEDIAN ABSOLUTE DEVIATION #########
+#consistent estimator of the std deviation.
+
+def mad(data):
+    return np.ma.median(np.abs(data - np.ma.median(data)))
+
+
+######### MATCHING PURSUIT - NO CRITERION #########
 
 def mp_arbitrary_criterion(phi, phi_t, y, n_iter):
     x = np.zeros(phi_t.dot(y).shape[0])
@@ -22,7 +30,7 @@ def mp_arbitrary_criterion(phi, phi_t, y, n_iter):
     return z, err, n_iter
 
 
-######### MATCHING PURSUIT, STATISTICAL CRITERION #########
+######### MATCHING PURSUIT - STATISTICAL CRITERION #########
 
 def mp_stat_criterion(phi, phi_t, y, sigma):
     p = phi_t.dot(y).shape[0]
@@ -52,7 +60,7 @@ def mp_stat_criterion(phi, phi_t, y, sigma):
         h = dic_arr.dot(np.linalg.inv(dic_arr.T.dot(dic_arr))).dot(dic_arr.T)
         t = np.abs(x.T.dot(y-h.dot(y)))/(sigma*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
 
-        s = np.random.standard_t(int(y.shape[0]-dic_arr.shape[1])-1, 50)
+        s = np.random.standard_t(int(y.shape[0]-dic_arr.shape[1]), 50)
         q = np.percentile(s, 95)
 
         dic.append(x)
@@ -62,7 +70,7 @@ def mp_stat_criterion(phi, phi_t, y, sigma):
     return phi.dot(sparse), err, dic_arr.shape[1]
 
 
-######### ORTHOGONAL MATCHING PURSUIT, STAT CRITERION #########
+######### ORTHOGONAL MATCHING PURSUIT - STAT CRITERION #########
 
 def omp_stat_criterion(phi, phi_t, y, sigma):
     p = phi_t.dot(y).shape[0]
@@ -93,7 +101,7 @@ def omp_stat_criterion(phi, phi_t, y, sigma):
         h = dic_arr.dot(np.linalg.inv(dic_arr.T.dot(dic_arr))).dot(dic_arr.T)
         t = np.abs(x.T.dot(y-h.dot(y)))/(sigma*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
 
-        s = np.random.standard_t(int(y.shape[0]-dic_arr.shape[1])-1, 50)
+        s = np.random.standard_t(int(y.shape[0]-dic_arr.shape[1]), 50)
         q = np.percentile(s, 95)
 
         dic.append(x)
@@ -103,13 +111,90 @@ def omp_stat_criterion(phi, phi_t, y, sigma):
     return phi.dot(sparse), err, dic_arr.shape[1]
 
 
-######### MEDIAN ABSOLUTE DEVIATION ######### 
+######### MP, OMP - STAT CRIT. W/ ENDOGENOUS VARIANCE  #########
 
-#consistent estimator of the std deviation
-#where hat(Sigma) a consistent estimator of the std dev. of the finest level detail coefficients.
+def mp_stat_endogen_var(phi, phi_t, y):
+    p = phi_t.dot(y).shape[0]
+    sparse = np.zeros(p)
+    err = [np.linalg.norm(y)]
 
-def mad(data):
-    return np.ma.median(np.abs(data - np.ma.median(data)))
+    c = phi_t.dot(y)
+    abs_c = np.abs(c)
+    i_0 = np.argmax(abs_c)
+
+    dic = [phi.dot(np.identity(p)[:, i_0])]
+    dic_arr = np.asarray(dic).T
+    t, q = (2, 1)
+
+    while t > q:
+
+        sparse[i_0] += c[i_0]
+        err.append(np.linalg.norm(y - phi.dot(sparse)))
+
+        c = phi_t.dot(y - phi.dot(sparse))
+        abs_c = np.abs(c)
+        i_0 = np.argmax(abs_c)
+
+        x = phi.dot(np.identity(p)[:, i_0])
+        h = dic_arr.dot(np.linalg.inv(dic_arr.T.dot(dic_arr))).dot(dic_arr.T)
+
+        #Estimating residual std. deviation, using OLS variance estimator s²
+        s = sqrt(y.T.dot(y-h.dot(y))/(y.shape[0]-dic_arr.shape[1]))
+
+        t = np.abs(x.T.dot(y-h.dot(y)))/(s*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
+
+        t_dist = np.random.standard_t(int(y.shape[0]-dic_arr.shape[1]), 50)
+        q = np.percentile(t_dist, 95)
+
+        #print y.shape[0], dic_arr.shape[1], s, t, q
+
+        dic.append(x)
+        dic_arr = np.asarray(dic).T
+
+    return phi.dot(sparse), err, dic_arr.shape[1]
+
+
+def omp_stat_endogen_var(phi, phi_t, y):
+    p = phi_t.dot(y).shape[0]
+    sparse = np.zeros(p)
+    err = [np.linalg.norm(y)]
+
+    c = phi_t.dot(y)
+    abs_c = np.abs(c)
+    i_0 = np.argmax(abs_c)
+
+    dic = [phi.dot(np.identity(p)[:, i_0])]
+    dic_arr = np.asarray(dic).T
+    t, q = (2, 1)
+
+    while t > q:
+
+        sparse[i_0] += c[i_0]
+        index = np.flatnonzero(sparse).tolist()
+        sparse[index] = np.linalg.pinv(phi.dot(np.identity(p)[:, index])).dot(y)  # OMP projection
+        err.append(np.linalg.norm(y - phi.dot(sparse)))
+
+        c = phi_t.dot(y - phi.dot(sparse))
+        abs_c = np.abs(c)
+        i_0 = np.argmax(abs_c)
+
+        x = phi.dot(np.identity(p)[:, i_0])
+        h = dic_arr.dot(np.linalg.inv(dic_arr.T.dot(dic_arr))).dot(dic_arr.T)
+
+        #Estimating residual std. deviation, using OLS variance estimator s²
+        sigma_est = sqrt(y.T.dot(y-h.dot(y))/(y.shape[0]-dic_arr.shape[1]))
+
+        t = np.abs(x.T.dot(y-h.dot(y)))/(sigma_est*sqrt(np.linalg.norm(x)-x.T.dot(h).dot(x)))
+
+        t_dist = np.random.standard_t(int(y.shape[0]-dic_arr.shape[1]), 50)
+        q = np.percentile(t_dist, 95)
+
+        #print y.shape[0], dic_arr.shape[1], sigma_est, t, q
+
+        dic.append(x)
+        dic_arr = np.asarray(dic).T
+
+    return phi.dot(sparse), err, dic_arr.shape[1]
 
 
 ######### HARD THRESHOLDING #########
@@ -120,7 +205,6 @@ def hard_thresholding(phi, phi_t, y, n_thresh):
     c = phi_t.dot(y)
     thr = sorted(np.abs(c), reverse=True)[n_thresh-1]
     c = pywt.thresholding.hard(c, thr)
-    #print np.flatnonzero(c).shape[0]
     return phi.dot(c)
 
 #b) Hard Thresholding, using MP on pre-experiment noise :
